b99d478c53f385296f46acc6524bdc48ab69f4a9
Genta Kurniawan <mgentakurniawan6@gmail.com>
2026-01-31 02:50:04 +0700

    fix(config): update config next js

--- brain-base-frontend/next.config.ts
+++ brain-base-frontend/next.config.ts
@@ -1,14 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = config.externals || [];
-      config.externals.push('dompurify');
-    }
-    return config;
-  },
   async headers() {
     return [
       {
